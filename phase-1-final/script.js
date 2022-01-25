5fca8d9fe995ef0427babadf4c6b41ddd65c481c
Kevin Wang <kevqwang@gmail.com>
2022-01-25 14:18:57 -0500

    3rd commit

--- phase-1-final/script.js
+++ phase-1-final/script.js
@@ -1,28 +1,32 @@
-let stock = 'IPOF'
+//let stock = 'IPOF'
 //let apiKey = '77bee9041a7ec2307b9a6644fb2d307c'
 //let  apiKey = '5d15681bbb513c48cd0a23a590947092'
 let apiKey = '7f245f5bc1f73dd003ca66b034f06f81'
 //let apiKey = '2a33673e3601176f857cb30e5207f88d'
 
-let url = `https://financialmodelingprep.com/api/v3/quote/${stock}?apikey=${apiKey}`
+//let url = `https://financialmodelingprep.com/api/v3/quote/${stock}?apikey=${apiKey}`
 
 const priceContainer = document.getElementById('priceContainer')
 
 
-queryStock();
+// queryStock();
 
-function queryStock() {
-fetch(url)
+function queryStock(ticker) {
+fetch(`https://financialmodelingprep.com/api/v3/quote/${ticker}?apikey=${apiKey}`)
 .then(resp => resp.json())
-.then(stock => renderPrice(stock))
+.then(stock => {
+  renderPrice(stock)
+  //buyFunction(stock[0].price)
+})
 } 
 
 
-
+let stockPrice = 0
 function renderPrice(stock) {
-  let stockPrice = stock[0].price
+  stockPrice = stock[0].price
   stockPrice = Math.round(100*stockPrice)/100;
   priceContainer.textContent = '$ ' + stockPrice;
+  
 
 }
 
@@ -33,12 +37,17 @@ function userInput(stock) {
 
   searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(inputField.value.toUpperCase())
+    queryStock(inputField.value.toUpperCase())
   })
 }
 
 
 
+// const buyInput = document.querySelector("buy")
+
+// function buyFunction(stockPrice) {
+//   buyInput.value = stockPrice
+//}
 
 
 
